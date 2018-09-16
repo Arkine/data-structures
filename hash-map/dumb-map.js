2bc3c9754fb18ac143bc2586f25c5dd3195fe9d6
arkine <slyngstad55@gmail.com>
2018-09-16 10:04:14 -0600

    added hash map

---
+++ hash-map/dumb-map.js
@@ -0,0 +1,53 @@
+const hash = require('string-hash'); // I could use  this but with symbols we get 0 collisions.
+
+class DumbMap {
+    constructor() {
+        this.entries = [];
+    }
+    get(x) {
+        const val = hash(x);
+        if (!this.entries[val]) {
+            return undefined;
+        }
+
+        let result;
+        this.entries[val].forEach(pair => {
+            if (pair[0] === val) {
+                result = pair[1];
+            }
+        });
+
+        return result;
+        // return this.entries[Symbol(x)]; // With symbols
+    }
+
+    set(x, y) {
+        const val = hash
+        if (!this.entries[val]) {
+            this.entries[val] = [];
+        }
+
+        let result;
+        this.entries[val].push([x, y]);
+    //    return this.entries[Symbol(x)] = y; // With symbols
+    }
+}
+
+let m = new DumbMap()
+m.set('x', 1)
+m.set('y', 2)
+
+console.time('with very few records in the map')
+m.get('I_DONT_EXIST')
+console.timeEnd('with very few records in the map')
+
+m = new DumbMap()
+
+for (x = 0; x < 1000000; x++) {
+  m.set(`element${x}`, x)
+}
+
+console.time('with lots of records in the map')
+m.get('I_DONT_EXIST')
+console.timeEnd('with lots of records in the map')
+
